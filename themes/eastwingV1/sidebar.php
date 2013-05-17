e85bd856fc8728365147afbcaab3cb3a21bf7500
Tim Smith <tim@timothybsmith.com>
2013-05-17 12:52:03 -0500

    Adding README and deleting unnecessary files

--- themes/eastwingV1/sidebar.php
+++
@@ -1,9 +0,0 @@
-<div class="ad">
-   <img src="http://placehold.it/150x100" width="150" height="100" />
-   <p><span class="adtag"><a href="#">Product Name</a></span>This is where we give you an up to 60 character product or service description.</p>
-</div><!-- .ad -->
-
-
-<div class="threecol last right">
-  <?php get_sidebar(); ?>
-</div>
--- themes/eastwingV2/sidebar.php
+++
@@ -1,5 +0,0 @@
-<h3>Sponsored By</h3>
-<div class="ad">
-   <a href="http://www.git-tower.com/?utm_source=theeastwing&utm_medium=banner&utm_campaign=sponsoring_june2012" target="_blank"><img src="http://theeastwing.net/wp-content/uploads/2012/06/Tower_150x100.jpeg" width="150" height="100" /></a>
-   <p><span class="adtag"><a href="http://www.git-tower.com/?utm_source=theeastwing&utm_medium=banner&utm_campaign=sponsoring_june2012" target="_blank">Tower</a></span>Tower is the most powerful Git client for Mac. Download the free trial!</p>
-</div><!-- .ad -->
