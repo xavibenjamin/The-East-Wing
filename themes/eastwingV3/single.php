57d7dde0f330d9e2ae4ae1084a2b2a9ee5d3f865
Tim Smith <tim@timothybsmith.com>
2013-06-19 01:10:26 -0500

    Changing styling around previous and next nav

--- themes/eastwingV3/single.php
+++ themes/eastwingV3/single.php
@@ -20,7 +20,17 @@
       <ul class="meta_info">
         <li><?php the_tags('#', ' #'); ?></li>
         <li><a href="http://twitter.com/home?status=Currently listening to <?php the_permalink(); ?> /via @the_eastwing" title="Share on Twitter" target="_blank">Share on Twitter</a> &middot; <a href="http://www.facebook.com/sharer.php?u=<?php the_permalink();?>&t=<?php the_title(); ?>" title="Share on Facebook" target="blank">Share on Facebook</a></li>
-        <li><?php previous_post_link('%link', 'Previous', TRUE); ?> &middot; <?php next_post_link('%link', 'Next', TRUE); ?></li>
+        <li>
+          <?php previous_post_link('%link', 'Previous Episode', TRUE); ?>
+          <?php if(!get_adjacent_post(true, '', true)) { 
+            echo '<span class="inactive">Previous Episode</span>'; 
+          } ?>
+          &middot;
+          <?php next_post_link('%link', 'Next Episode', TRUE); ?>
+          <?php if(!get_adjacent_post(true, '', false)) { 
+            echo '<span class="inactive">Next Episode</span>'; 
+          } ?>
+        </li>
       </ul>
     </div> <!-- end .eight -->
 
