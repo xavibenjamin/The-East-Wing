62636ac211ea284fc87a3d7c6924e99dcf4aeccd
Tim Smith <tim@timothybsmith.com>
2013-07-03 17:19:08 -0500

    Writing Markup for episodes on our index page, and page navigation.

--- themes/theeastwingv4/index.php
+++ themes/theeastwingv4/index.php
@@ -1,23 +1,23 @@
 <?php get_header('home'); ?>
 
+<section class="main-content" role="main">
+  <div class="contain">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+      <article class="entry">
+        <?php the_post_thumbnail( 'cover-art' ); ?>
+        <h2><a href="<?php the_permalink(); ?>" title="Permanent Link to <?php the_title(); ?>"><?php the_title();?></a></h2>
+        <p class="pubdate"><time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('F jS, Y');?></time></p>
+        <?php the_excerpt(); ?>
 
-<div class="grid_max">
-    <?php if (have_posts()) : ?>
-      <?php while (have_posts()) : the_post(); ?>
+      </article><!-- .entry -->
+    <?php endwhile; endif; ?>
+  </div><!-- .contain -->
+</section>
 
-<div class="three left entry">
-    <a href="<?php the_permalink() ?>" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_post_thumbnail( 'cover-art' ); ?><h2><?php the_title(); ?></h2></a>
+<section class="page-navigation">
+  <div class="contain">
+    <?php wp_pagenavi(); ?>
+  </div>
+</section>
 
-    <p class="date">Posted on <?php the_time('F j, Y');?></p>
-
-</div> <!-- end .entry -->
-    
-<?php endwhile; ?>
-<?php endif; ?>
-
-<div class="twelve">
-      <?php wp_pagenavi(); ?>
-</div><!-- .twelve -->
-
-</div> <!-- end .grid_max -->
 <?php get_footer(); ?>
