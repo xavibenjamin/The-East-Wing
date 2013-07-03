58a4715be66b2b7d9cb4ba356597ec4e8580db8d
Tim Smith <tim@timothybsmith.com>
2013-07-03 16:47:16 -0500

    Styled Guest-Sponsor area. Renamed "colors" to "bits" and removed Guest-Sponsor area from index.php to header-home.php

--- themes/theeastwingv4/header-home.php
+++ themes/theeastwingv4/header-home.php
@@ -74,6 +74,50 @@
   
 </header>
 
+<section class="guest-sponsor-bar">
+  <div class="contain">
+
+    <?php 
+      $the_query = new WP_Query(array(
+        'post_type' => 'upcoming-guests',
+        'showposts' => '1'   
+      )); 
+      ?>
+
+      <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+
+        <div class="guest-info">
+          <h2>Next on The East Wing</h2>
+          <?php the_post_thumbnail(); ?>
+          <h3><a href="<?php the_field('guest_url'); ?>"><?php the_title(); ?></a></h3>
+          <p>Airing on: <span><?php $date = DateTime::createFromFormat('Ymd', get_field('air_date')); echo $date->format('F jS, Y'); ?></span></p>
+        </div>
+      
+      <?php wp_reset_postdata(); endwhile; ?>
+
+      
+      <?php 
+      $the_query = new WP_Query(array(
+        'post_type' => 'sponsors',
+        'showposts' => '1'   
+      )); 
+      ?>
+
+      <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+
+      <div class="adspot">
+        <h2>Sponsored By</h2>
+        <?php the_post_thumbnail(); ?>
+        <h3><a href="<?php the_field('ad_url'); ?>"><?php the_title(); ?></a></h3>
+        <?php the_content(); ?>
+      </div>
+      
+      <?php wp_reset_postdata(); endwhile; ?>
+
+
+  </div><!-- end .contain -->
+</section>
+
 
 
 
--- themes/theeastwingv4/index.php
+++ themes/theeastwingv4/index.php
@@ -1,50 +1,6 @@
 <?php get_header('home'); ?>
 
 
-<section class="guest-sponsor-bar">
-  <div class="contain">
-
-    <?php 
-      $the_query = new WP_Query(array(
-        'post_type' => 'upcoming-guests',
-        'showposts' => '1'   
-      )); 
-      ?>
-
-      <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
-
-        <div class="guest-info">
-          <h2>Next on The East Wing</h2>
-          <?php the_post_thumbnail(); ?>
-          <h3><a href="<?php the_field('guest_url'); ?>"><?php the_title(); ?></a></h3>
-          <p>Airing the week of: <span><?php $date = DateTime::createFromFormat('Ymd', get_field('air_date')); echo $date->format('F jS, Y'); ?></span></p>
-        </div>
-      
-      <?php wp_reset_postdata(); endwhile; ?>
-
-      
-      <?php 
-      $the_query = new WP_Query(array(
-        'post_type' => 'sponsors',
-        'showposts' => '1'   
-      )); 
-      ?>
-
-      <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
-
-      <div class="adspot">
-        <h2>Sponsored By</h2>
-        <?php the_post_thumbnail(); ?>
-        <h3><a href="<?php the_field('ad_url'); ?>"><?php the_title(); ?></a></h3>
-        <p><?php the_content(); ?></p>
-      </div>
-      
-      <?php wp_reset_postdata(); endwhile; ?>
-
-
-  </div><!-- end .contain -->
-</section>
-
 <div class="grid_max">
     <?php if (have_posts()) : ?>
       <?php while (have_posts()) : the_post(); ?>
