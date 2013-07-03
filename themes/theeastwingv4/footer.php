1ff20446d4e3790d93452852fc54cf8d58222bc4
Tim Smith <tim@timothybsmith.com>
2013-07-03 18:49:08 -0500

    Styled footer, added styling for em's, brought in picture of me

--- themes/theeastwingv4/footer.php
+++ themes/theeastwingv4/footer.php
@@ -1,23 +1,29 @@
 <footer role="contentinfo">
   <section class="about-the-host">
-    <img src="" alt="">
-    <div class="text">
-      <h2>About the Host</h2>
-      <h3>Hi! I’m Tim Smith, a Designer, Talker &amp; Coffee Addict.</h3>
-      <p>I run a small design studio called <a href="http://anythingo.es/">Anythin’ Goes</a> and I teach Web Design, Front-End Development, and more on <a href="http://timlikestoteach.com/">Tim Likes to Teach</a>.</p>
-      <p><a href="http://ttimsmith.com">ttimsmith.com</a></p>
-    </div>
+    <div class="contain">
+      <img src="<?php bloginfo('template_url'); ?>/assets/img/tim_400.png" alt="Tim Smith">
+      <div class="text">
+        <h2>About the Host</h2>
+        <h3>Hi! I’m Tim Smith, a Designer, Talker &amp; Coffee Addict.</h3>
+        <p>I run a small design studio called <a href="http://anythingo.es/">Anythin’ Goes</a> and I teach Web Design, Front-End Development, and more on <a href="http://timlikestoteach.com/">Tim Likes to Teach</a>.</p>
+        <p><a href="http://ttimsmith.com">ttimsmith.com</a></p>
+      </div>
+    </div><!-- .contain -->
   </section><!-- .about-the-host -->
 
   <section class="helpful-links">
-   <p><a href="https://twitter.com/the_eastwing" class="twitter-follow-button" data-show-count="false">Follow @the_eastwing</a>
-    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></p>
-   <p><a href="/live">Live Stream</a> &middot; <a href="/schedule">Broadcast Schedule</a></p>
-   <p><a href="http://itunes.apple.com/us/podcast/the-east-wing/id503801143" target="_blank">Subscribe on iTunes</a> &middot; <a href="http://theeastwing.net/feed/podcast/" target="_blank">Subscribe via RSS</a></p>
+    <div class="contain">
+      <p><a href="https://twitter.com/the_eastwing" class="twitter-follow-button" data-show-count="false">Follow @the_eastwing</a>
+      <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></p>
+      <p><a href="/live">Live Stream</a> &middot; <a href="/schedule">Broadcast Schedule</a></p>
+      <p><a href="http://itunes.apple.com/us/podcast/the-east-wing/id503801143" target="_blank">Subscribe on iTunes</a> &middot; <a href="http://theeastwing.net/feed/podcast/" target="_blank">Subscribe via RSS</a></p>
+    </div><!-- .contain -->
   </section><!-- .helpful-links -->
 
   <section class="copyright">
-    <small><em>The East Wing</em> &copy; 2012&ndash;<?php echo date('Y')?> <a href="http://anythingo.es">Anythin&rsquo; Goes, LLC</a>. Hosted by <a href="http://www.mediatemple.net/#a_aid=4f42c6d7d53a5">Media Temple</a></small>
+    <div class="contain">
+      <small><em>The East Wing</em> &copy; 2012&ndash;<?php echo date('Y')?> <a href="http://anythingo.es">Anythin&rsquo; Goes, LLC</a>. Hosted by <a href="http://www.mediatemple.net/#a_aid=4f42c6d7d53a5">Media Temple</a></small>
+    </div><!-- .contain -->
   </section>
 
 </footer>
