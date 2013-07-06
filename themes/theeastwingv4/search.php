bc6351ab18f2299f24c2b74a3d3943bf4f4f1265
Tim Smith <tim@timothybsmith.com>
2013-07-05 21:35:45 -0500

    Styled the search page. Duh!

--- themes/theeastwingv4/search.php
+++ themes/theeastwingv4/search.php
@@ -1,29 +1,43 @@
-<?php get_header() ?>
- 
- <div class="grid_max">
-   
-   <div class="twelve"><h1 class="tax-head">Search Results</h1></div>
- 
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>  
- 
-      <div class="three left entry">
-      
-        <a href="<?php the_permalink() ?>" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_post_thumbnail( 'cover-art' ); ?><h2><?php the_title(); ?></h2></a>
+<?php get_header(); ?>
 
-        <p class="date">Posted on <?php the_time('F j, Y');?></p>
+<section class="main-content" role="main">
+  <div class="contain">
+    <h2 class="page-title">Search Results For: <?php the_search_query(); ?></h2>
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+      <article class="entry">
+        <?php the_post_thumbnail( 'cover-art' ); ?>
+        <div class="text">
+          <p class="pubdate">
+            <span class="screen-reader-text">Calendar Icon</span>
+            <span class="icon" data-icon="c"></span>
+            <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('F jS, Y');?></time>
+          </p>
+          <h2><a href="<?php the_permalink(); ?>" title="Permanent Link to <?php the_title(); ?>">#<?php the_field('episode_number'); ?>: <?php the_title();?></a></h2>
+          <?php the_excerpt(); ?>
+        </div><!-- .text -->
 
-      </div> <!-- end .entry -->
- 
-     <?php endwhile;?>
-     
-     <?php else : ?>
-       
-      <div class="seven entry" <?php post_class(); ?>>
-      <p>No posts found. Try a different search?</p>
-      </div><!-- .entry -->
-
-    <?php endif; ?>
+      </article><!-- .entry -->
+    <?php endwhile; ?>
     
-</div><!-- .grid_max -->
+    <?php else : ?>
+      <div class="text">
+        <p>No posts found. Try a different search?</p>
+      </div>
+    <?php endif; ?>  
+
+
+
+
+  </div><!-- .contain -->
+
+  <section class="page-navigation">
+    <div class="contain">
+      <?php wp_pagenavi(); ?>
+    </div>
+  </section>
+
+</section><!-- .main-content -->
+
+
 
 <?php get_footer(); ?>
