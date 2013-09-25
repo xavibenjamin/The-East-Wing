d3a6ecbf819333c0d078aec55ba1ec48476df07c
Tim Smith <smith@ttimsmith.com>
2013-09-25 14:20:47 -0500

    Added Fusion ad

--- themes/theeastwingv4/header-home.php
+++ themes/theeastwingv4/header-home.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <head>
 
-  <!-- 
+  <!--
 
-  @@@@@@@@@@@@@@@@@  @@@   @@@          @@@ 
+  @@@@@@@@@@@@@@@@@  @@@   @@@          @@@
   @@@@@@@@@@@@@@@@@  @@@   @@@@        @@@@
          @@@         @@@   @@@ @      @ @@@
          @@@         @@@   @@@  @@  @@  @@@
@@ -21,25 +21,35 @@
 
 	<meta charset="utf-8" />
 	<meta name="description" content="The East Wing is a weekly interview show featuring established and up-and-coming designers, developers, and entrepreneurs." />
-	
+
   <title><?php is_home() ? bloginfo('description') : wp_title(''); ?></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 	<link rel="alternate" type="application/rss+xml" href="<?php bloginfo('rss2_url'); ?>" />
 
   <link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/favicon.ico">
-	  
+
   <!-- Global -->
-  <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/assets/css/global.css"/> 
+  <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/assets/css/global.css"/>
 
   <!-- Typekit Code -->
   <script type="text/javascript" src="//use.typekit.net/ohk1cik.js"></script>
-  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>  
+  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
+  <!-- Fusion Ad Code -->
+  <script type="text/javascript">
+  (function(){
+    var fusion = document.createElement('script');
+    fusion.src = window.location.protocol + '//adn.fusionads.net/api/1.0/ad.js?zoneid=294&rand=' + Math.floor(Math.random()*9999999);
+    fusion.async = true;
+    (document.head || document.getElementsByTagName('head')[0]).appendChild(fusion);
+  })();</script>
+
 
   <!--WP Generated Header -->
   <?php wp_head(); ?>
   <!--End WP Generated Header -->
-  
+
 </head>
 
 
@@ -60,14 +70,14 @@
 
 <header role="banner">
   <section class="nav-bar">
-    <div class="contain">    
+    <div class="contain">
       <nav role="navigation" class="header-nav">
         <a href="/">Episodes</a>
         <a href="/contact">Contact</a>
         <a href="/sponsorship">Sponsorship</a>
-        <a href="/membership">Membership</a>    
+        <a href="/membership">Membership</a>
       </nav>
-      
+
       <div class="subscribe-search">
         <ul class="subscribe">
           <li>Subscribe:</li>
@@ -94,7 +104,7 @@
       <a href="/" class="logo">The East Wing</a>
     </div> <!-- end .contain -->
   </section><!-- end .statement -->
-  
+
 </header>
 
 <section class="alt-description">
@@ -106,30 +116,16 @@
 
 <section class="guest-sponsor-bar">
   <div class="contain">
-      
-      <?php 
-      $the_query = new WP_Query(array(
-        'post_type' => 'sponsors',
-        'showposts' => '1'   
-      )); 
-      ?>
 
-      <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
-
-      <div class="adspot">
-        <h2>Sponsored By</h2>
-        <?php the_post_thumbnail(); ?>
-        <h3><a href="<?php the_field('ad_url'); ?>"><?php the_title(); ?></a></h3>
-        <?php the_content(); ?>
+      <div id="fusion_ad" class="adspot">
+        <a class="fusionlink" href="http://fusionads.net">Powered by Fusion</a>
       </div>
-      
-      <?php wp_reset_postdata(); endwhile; ?>
 
-      <?php 
+      <?php
       $the_query = new WP_Query(array(
         'post_type' => 'upcoming-guests',
-        'showposts' => '1'   
-      )); 
+        'showposts' => '1'
+      ));
       ?>
 
       <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
@@ -140,7 +136,7 @@
           <h3><a href="<?php the_field('guest_url'); ?>"><?php the_title(); ?></a></h3>
           <p>Airing on: <span><?php $date = DateTime::createFromFormat('Ymd', get_field('air_date')); echo $date->format('F jS, Y'); ?></span></p>
         </div>
-      
+
       <?php wp_reset_postdata(); endwhile; ?>
 
 
--- themes/theeastwingv4/header.php
+++ themes/theeastwingv4/header.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <head>
-  
-  <!-- 
 
-  @@@@@@@@@@@@@@@@@  @@@   @@@          @@@ 
+  <!--
+
+  @@@@@@@@@@@@@@@@@  @@@   @@@          @@@
   @@@@@@@@@@@@@@@@@  @@@   @@@@        @@@@
          @@@         @@@   @@@ @      @ @@@
          @@@         @@@   @@@  @@  @@  @@@
@@ -21,25 +21,34 @@
 
   <meta charset="utf-8" />
   <meta name="description" content="The East Wing is a podcast brought to you by Tim Smith, that talks with industry experts about design, solving problems and the keys to creating products with value." />
-  
+
   <title><?php is_home() ? bloginfo('description') : wp_title(''); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
   <link rel="alternate" type="application/rss+xml" href="<?php bloginfo('rss2_url'); ?>" />
 
   <link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/favicon.ico">
-    
+
   <!-- Global -->
-  <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/assets/css/global.css"/> 
+  <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/assets/css/global.css"/>
 
   <!-- Typekit Code -->
   <script type="text/javascript" src="//use.typekit.net/ohk1cik.js"></script>
-  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>  
+  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
+  <!-- Fusion Ad Code -->
+  <script type="text/javascript">
+  (function(){
+    var fusion = document.createElement('script');
+    fusion.src = window.location.protocol + '//adn.fusionads.net/api/1.0/ad.js?zoneid=294&rand=' + Math.floor(Math.random()*9999999);
+    fusion.async = true;
+    (document.head || document.getElementsByTagName('head')[0]).appendChild(fusion);
+  })();</script>
 
   <!--WP Generated Header -->
   <?php wp_head(); ?>
   <!--End WP Generated Header -->
-  
+
 </head>
 
 
@@ -60,14 +69,14 @@
 
 <header role="banner">
   <section class="nav-bar">
-    <div class="contain">    
+    <div class="contain">
       <nav role="navigation" class="header-nav">
         <a href="/">Episodes</a>
         <a href="/contact">Contact</a>
         <a href="/sponsorship">Sponsorship</a>
-        <a href="/membership">Membership</a>    
+        <a href="/membership">Membership</a>
       </nav>
-      
+
       <div class="subscribe-search">
         <ul class="subscribe">
           <li>Subscribe:</li>
@@ -94,35 +103,21 @@
       <a href="/" class="logo-alt">The East Wing</a>
     </div> <!-- end .contain -->
   </section><!-- end .statement -->
-  
+
 </header>
 
 <section class="guest-sponsor-bar with-background">
   <div class="contain">
-      
-      <?php 
-      $the_query = new WP_Query(array(
-        'post_type' => 'sponsors',
-        'showposts' => '1'   
-      )); 
-      ?>
 
-      <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
-
-      <div class="adspot">
-        <h2>Sponsored By</h2>
-        <?php the_post_thumbnail(); ?>
-        <h3><a href="<?php the_field('ad_url'); ?>"><?php the_title(); ?></a></h3>
-        <?php the_content(); ?>
+      <div id="fusion_ad" class="adspot">
+        <a class="fusionlink" href="http://fusionads.net">Powered by Fusion</a>
       </div>
-      
-      <?php wp_reset_postdata(); endwhile; ?>
 
-      <?php 
+      <?php
       $the_query = new WP_Query(array(
         'post_type' => 'upcoming-guests',
-        'showposts' => '1'   
-      )); 
+        'showposts' => '1'
+      ));
       ?>
 
       <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
@@ -133,7 +128,7 @@
           <h3><a href="<?php the_field('guest_url'); ?>"><?php the_title(); ?></a></h3>
           <p>Airing on: <span><?php $date = DateTime::createFromFormat('Ymd', get_field('air_date')); echo $date->format('F jS, Y'); ?></span></p>
         </div>
-      
+
       <?php wp_reset_postdata(); endwhile; ?>
 
 
