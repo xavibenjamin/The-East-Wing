07c915a08fcd2903efd9f8943da5e66965d6d723
Tim Smith <smith@ttimsmith.com>
2014-01-07 00:35:24 -0600

    Changing meta viewport

--- themes/theeastwingv5/header.php
+++ themes/theeastwingv5/header.php
@@ -24,7 +24,7 @@
 
   <meta charset="utf-8" />
   <title><?php is_home() ? bloginfo('description') : wp_title(''); ?></title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="viewport" content="width=device-width" />
 
 
   <!-- My styles -->
